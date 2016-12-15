ce1149186bef634657e9393e7146331eb2fc85fd
Friðrik Hjálmarsson <fridrik14@ru.is>
2016-12-15 18:53:05 +0000

    meira dot

--- klasar.py
+++ klasar.py
@@ -1,11 +1,14 @@
 class Game:
-	current = 0
-	def __init__(self):
-		self.players = []
-		self.category = ''
+    current = 0
+    turn = 1
+
+    def __init__(self):
+        self.players = []
+        self.category = ''
+
 
 class Player:
-	def __init__(self):
-		self.name = ''
-		self.id = 0
-		self.score = 0
+    def __init__(self):
+        self.name = ''
+        self.id = 0
+        self.score = 0
